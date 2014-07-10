04de47dd9b2843e7a3bacb1688d9e8783036b828
Emmanuel Freard <e.freard@vi-live.fr>
2014-07-10 12:25:19 +0200

    Update 2014-07-10T10:25:19.896Z

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var packageJson = require('./package.json');
-var gutil = require('gulp-util');
-var loadTasks = require('module-boilerplate');
-
-loadTasks(gulp, packageJson);
